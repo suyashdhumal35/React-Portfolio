e272e799225316a1c060d29fb360df53bd4781f2
suyashdhumal35 <suyashdhumal35@gmail.com>
2025-03-17 20:28:52 +0530

    new one added

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,20 +2,27 @@
 export const projects = [
   {
     id: 1,
+    title: "Food-Restro",
+    description: "A modern e-commerce site built with React JS , Tailwind CSS , Etc",
+    image: "https://images.unsplash.com/photo-1490645935967-10de6ba17061?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGZvb2QlMjB3ZWIlMjBzaXRlfGVufDB8fDB8fHww",
+    link: "https://dhumalsrestro.netlify.app/",
+  },
+  {
+    id: 2,
     title: "HexaShop",
     description: "A modern e-commerce site built with HTML, CSS, and Bootstrap.",
     image: "https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=600",
     link: "https://e-commerece-hexashop.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Cryptop",
     description: "A cryptocurrency tracking website displaying real-time updates.",
     image: "https://images.pexels.com/photos/373076/pexels-photo-373076.jpeg?auto=compress&cs=tinysrgb&w=600",
     link: "https://suyash-cryptop.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Portfolio",
     description: "A personal portfolio website showcasing my projects and skills.",
     image: "https://images.pexels.com/photos/3183197/pexels-photo-3183197.jpeg?auto=compress&cs=tinysrgb&w=600",
